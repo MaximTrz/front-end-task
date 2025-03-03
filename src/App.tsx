86be983a820ce64ff7d8035778cbd37d005fe305
Maxim <maxim.trz@gmail.com>
2025-03-03 10:49:08 +0500

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.scss';
+
+// function App() {
+//   return <></>;
+// }
+
+const App: React.FC = () => {
+  return <>Hello</>;
+};
+
+export default App;
